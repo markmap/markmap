cef404579718a760a57c5fedac5af1cf5cfed166
Gerald <gera2ld@163.com>
2020-06-12 12:32:35 +0800

    fix: set font-weight to 600 for <strong>

--- src/view.ts
+++ src/view.ts
@@ -73,7 +73,7 @@ export class Markmap {
 .${id}-fo code { padding: .2em .4em; font-size: calc(1em - 2px); color: #555; background-color: #f0f0f0; border-radius: 2px; }
 .${id}-fo del { text-decoration: line-through; }
 .${id}-fo em { font-style: italic; }
-.${id}-fo strong { font-weight: 500; }
+.${id}-fo strong { font-weight: 600; }
 .${id}-fo pre { margin: 0; }
 .${id}-fo pre[class*=language-] { padding: 0; }
 .${id}-g > g { cursor: pointer; }
