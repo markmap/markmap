39561ff692e256f5f87896dc290e779dcede0d86
ilyes-ced <random_dude_233@proton.me>
2023-12-30 20:19:35 +0000

    add checkboxes to render

--- packages/markmap-lib/src/constants.ts
+++ packages/markmap-lib/src/constants.ts
@@ -4,3 +4,6 @@ export const baseJsPaths = [
   `d3@${process.env.D3_VERSION}/dist/d3.min.js`,
   `markmap-view@${process.env.VIEW_VERSION}/dist/browser/index.js`,
 ];
+
+export const svg_marked = `<svg class="change_color" fill="currentColor" width="16px" height="16px" viewBox="0 -3 24 24" data-darkreader-inline-fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path></g></svg>`;
+export const svg_unmarked = `<svg class="change_color" fill="currentColor" width="16px" height="16px" viewBox="0 -3 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5C5.44772 5 5 5.44772 5 6V13V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V13V6C19 5.44772 18.5523 5 18 5H6ZM3 6C3 4.34315 4.34315 3 6 3H18C19.6569 3 21 4.34315 21 6V13V18C21 19.6569 19.6569 21 18 21H6C4.34315 21 3 19.6569 3 18V13V6Z" data-darkreader-inline-fill=""></path> </g></svg>`;
