cf8121c8b064069bbba583e12565996eb09b81ab
Gerald <code@gera2ld.space>
2024-01-23 15:12:02 +0800

    fix: fix lib version

--- packages/markmap-lib/src/index.ts
+++ packages/markmap-lib/src/index.ts
@@ -3,6 +3,6 @@ export * from './constants';
 export * from './transform';
 
 export const transformerVersions = {
-  'markmap-lib': 'process.env.VERSION',
+  'markmap-lib': 'process.env.LIB_VERSION',
   d3: process.env.D3_VERSION,
 };
